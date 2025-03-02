705787185bf4b8e13fcab2cd64bc1bce2f6b86e2
Arnav-Bista <arnav.bista12@gmail.com>
2025-03-02 15:22:04 +0000

    Added next config URL

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_STRAPI_URL: 'http://localhost:1337',
+  },
 };
 
 export default nextConfig;
