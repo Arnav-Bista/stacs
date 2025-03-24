7ab8b9bf6a309856823e94efe3fd4109a8227cc2
Arnav-Bista <arnav.bista12@gmail.com>
2025-03-24 19:54:58 +0000

    Better Logo

--- frontend/app/components/StacsLogo.tsx
+++ frontend/app/components/StacsLogo.tsx
@@ -14,12 +14,26 @@ type Point = [number, number];
 
 const points: Array<Point> = [[0, 0], [1, 0], [2, 0], [2, 1], [1, 1], [0, 1], [0, 2], [1, 2], [2, 2], [2, 3], [1, 3], [0, 3], [0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [5, 3], [4, 3], [3, 3], [3, 2], [4, 2], [5, 2], [5, 1], [4, 1], [3, 1], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [8, 1], [7, 1], [6, 1], [6, 2], [7, 2], [8, 2], [8, 3], [7, 3], [6, 3], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4], [11, 3], [10, 3], [9, 3], [9, 2], [10, 2], [11, 2], [11, 1], [10, 1], [9, 1], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0], [14, 0], [14, 1], [13, 1], [12, 1], [12, 2], [13, 2], [14, 2], [14, 3], [13, 3], [12, 3], [12, 4], [13, 4], [14, 4]];
 
+// Time for some not very nice coding (forgive me)
+
+const sPoints = [[-0.25, 0], [0, 0], [1, 0], [2, 0], [2, 1], [1, 1], [0, 1], [0, 2], [1, 2], [2.25, 2] ];
+
+const tPoints1 = [[2.75, 2], [4, 2], [5.25, 2]];
+const tPoints2 = [[4, 2], [4, -0.25]];
+
+const aPoints1 = [[6, -0.25], [6, 2], [7, 2], [8, 2], [8, 1], [8, -0.25]];
+const aPoints2 = [[6, 1], [7, 1], [8, 1]];
+
+const cPoints = [[11, 0], [10, 0], [9, 0], [9, 1], [9, 2], [10, 2], [11, 2]];
+
+const s2Points = [[11.75, 0], [13, 0], [14, 0], [14, 1], [13, 1], [12, 1], [12, 2], [13, 2], [14.25, 2]];
+
 export default function StacsLogo() {
   return (
-    <div className="flex flex-col items-center justify-center relative gap-2">
+    <div className="flex flex-col items-center justify-center relative">
       <svg
         viewBox="0 -0.5 14.5 5"
-        className="w-full h-32"
+        className="w-full h-28"
       >
         <path
           className="murray-path"
@@ -29,11 +43,19 @@ export default function StacsLogo() {
         />
       </svg>
 
-      <h1 className=" w-full text-[5rem] font-extrabold z-10 text-[#00527b] leading-none text-center font-['Odibee_Sans']">STACS</h1>
+      <svg viewBox="0 0.5 14.5 5" className="w-full h-28 my-[-1rem]">
+          <path className="stacs-letter" d={`M ${sPoints.map(point => `${point[0]} ${4 - point[1]}`).join(' L ')}`} />
+          <path className="stacs-letter" d={`M ${tPoints1.map(point => `${point[0]} ${4 - point[1]}`).join(' L ')}`} />
+          <path className="stacs-letter" d={`M ${tPoints2.map(point => `${point[0]} ${4 - point[1]}`).join(' L ')}`} />
+          <path className="stacs-letter" d={`M ${aPoints1.map(point => `${point[0]} ${4 - point[1]}`).join(' L ')}`} />
+          <path className="stacs-letter" d={`M ${aPoints2.map(point => `${point[0]} ${4 - point[1]}`).join(' L ')}`} />
+          <path className="stacs-letter" d={`M ${cPoints.map(point => `${point[0]} ${4 - point[1]}`).join(' L ')}`} />
+          <path className="stacs-letter" d={`M ${s2Points.map(point => `${point[0]} ${4 - point[1]}`).join(' L ')}`} />
+      </svg>
 
       <svg
         viewBox="0 -0.5 14.5 5"
-        className="w-full h-32"
+        className="w-full h-28"
       >
         <path
           className="murray-path"
